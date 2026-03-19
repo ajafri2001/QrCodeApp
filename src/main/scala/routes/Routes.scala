fb6ba2b585e314d25fd93edc93fbd9568affe3f7
Abdullah Arif Jafri <abdullahjafri2001@gmail.com>
2026-03-20 02:21:47 +0530

    Setup a working front-end

--- src/main/scala/routes/Routes.scala
+++ src/main/scala/routes/Routes.scala
@@ -6,7 +6,11 @@ import org.http4s.*
 import org.http4s.dsl.io.*
 
 object Routes:
+    object DataQueryParam extends QueryParamDecoderMatcher[String]("data")
+
     val routes: HttpRoutes[IO] =
         HttpRoutes.of[IO]:
-            case GET -> Root / "health" =>
+            case GET -> Root / "api" / "health" =>
                 Ok("Healthy")
+
+            case GET -> Root / "api" / "echo" :? DataQueryParam(data) => Ok(s"Here is the ${data}")
